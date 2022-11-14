dc95b2fd709c12bc6a4b0cca90d12e364eaacd22
RinseWillet <r.willet@gmail.com>
2022-11-14 22:06:25 +0100

    data.sql data inserting finally done

--- src/main/java/com/archaeologyprojects/strabomap/ancientreference/AncientReference.java
+++ src/main/java/com/archaeologyprojects/strabomap/ancientreference/AncientReference.java
@@ -13,7 +13,9 @@ public class AncientReference {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Enumerated(EnumType.ORDINAL)
     private TypeWork typeWork;
+
     private String author;
     private String title;
     private String reference;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
 delete from ancref;
 delete from modref;
 delete from settlement;
+drop TYPE if exists type_work ;
 
+create TYPE type_work as enum ('EPIGRAPHY', 'NUMISMATIC', 'TEXT', 'DEPICTION');
 insert into settlement (pleiades_id, ancient_name, modern_name, geom) values (999, 'test', 'test', 'SRID=4326; POINT(52.06 4.288)');
-insert into ancref (type_work, author, title, reference) values ('EPIGRAPHY', 'test_auth', 'test_titl', 'test_XI.I');
+insert into ancref (type_work, author, title, reference, settlement_id) values (1, 'test_auth', 'test_titl', 'test_XI.I', (SELECT id from settlement WHERE ancient_name='test'));
